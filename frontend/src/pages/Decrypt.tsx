1bc3b1d6111fa5b8543035b2e213785ac4447b75
Dasun Nethsara <techsaralk.pro@gmail.com>
2025-02-11 21:22:23 +0530

    add Decrypt and Encrypt components with NavBar integration

---
+++ frontend/src/pages/Decrypt.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavBar from '../Components/NavBar'
+
+function Decrypt() {
+    return (
+        <>
+            <NavBar />
+            <h1 className='display-6'>Decrypt</h1>
+        </>
+    )
+}
+
+export default Decrypt
---
+++ frontend/src/pages/Encrypt.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavBar from '../Components/NavBar'
+
+function Encrypt() {
+    return (
+        <>
+            <NavBar />
+            <h1 className='display-6'>Encrypt</h1>
+        </>
+    )
+}
+
+export default Encrypt
