641b177f51bffa4ce07482f27b0a3c9d6e660a29
Dasun Nethsara <techsaralk.pro@gmail.com>
2025-02-11 21:21:58 +0530

    implement routing with React Router and add Bootstrap for styling

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,34 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App.tsx'
+
+import Encrypt from './pages/encrypt.tsx'
+import Decrypt from './pages/decrypt.tsx'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.js'
+import 'bootstrap/dist/js/bootstrap.bundle.js'
+import 'bootstrap/dist/js/bootstrap.min.js'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/encrypt",
+    element: <Encrypt />,
+  },
+  {
+    path: "/decrypt",
+    element: <Decrypt />
+  }
+]);
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
