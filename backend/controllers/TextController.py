8164dacda847dad553baa8f4b8ba24fd1f4384bb
Dasun Nethsara <techsaralk.pro@gmail.com>
2025-02-12 12:41:21 +0530

    implement decryption functionality in TextController and add derotation method

--- backend/controllers/TextController.py
+++ backend/controllers/TextController.py
@@ -10,23 +10,39 @@ class TextController:
             cipher_text += rotated
         return cipher_text
 
-    def decrypt(self, text: str, key: int) -> None:
-        pass
+    def decrypt(self, text: str, key: int) -> str:
+        plain_text: str = ""
+        for char in text:
+            derotated: str = self.derotate(char, key)
+            plain_text += derotated
+        return plain_text
 
     def rotate(self, char: str, shift: int) -> str:
         # c = (p + k) % 26
         #       p -> character
         #       k -> position / shift
-        rotated = None
-        if not char.isalpha():
+        rotated: str | None = None
+        if (not char.isalpha()):
             rotated = char
         else:
             # check if the character if upper or lower
-            if(char.isupper()):
+            if char.isupper():
                 rotated_digit: int = (ord(char) - 65 + shift) % 26
-                rotated: str = chr(rotated_digit + 65)
-            elif(char.islower()):
+                rotated = chr(rotated_digit + 65)
+            elif char.islower():
                 rotated_digit: int = (ord(char) - 97 + shift) % 26
-                rotated: str = chr(rotated_digit + 97)
+                rotated = chr(rotated_digit + 97)
         return rotated
-        
+
+    def derotate(self, char: str, shift: int) -> str:
+        derotated: str | None = None
+        if not char.isalpha():
+            derotated = char
+        else:
+            if char.isupper():
+                derotated_digit: int = (ord(char) - 65 - shift) % 26
+                derotated = chr(derotated_digit + 65)
+            elif char.islower():
+                derotated_digit: int = (ord(char) - 97 - shift) % 26
+                derotated = chr(derotated_digit + 97)
+        return derotated
