66965136734f113d03771ae0e6e534758b81b7f5
Dasun Nethsara <techsaralk.pro@gmail.com>
2025-02-12 10:41:29 +0530

    add TextController class with methods for text rotation, encryption, and decryption

---
+++ backend/controllers/TextController.py
@@ -0,0 +1,13 @@
+class TextController:
+
+    def __init__(self) -> None:
+        pass
+
+    def rotate(self, text: str, key: int) -> None:
+        pass
+
+    def encrypt(self) -> None:
+        pass
+
+    def decrypt(self) -> None:
+        pass
