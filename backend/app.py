d931791ae75ac006b03c54e8cff73644b9f1079d
Dasun Nethsara <techsaralk.pro@gmail.com>
2025-02-12 12:10:19 +0530

    refactor app.py to initialize TextController and update encrypt_text endpoint to return cipher text

--- backend/app.py
+++ backend/app.py
@@ -1,23 +1,33 @@
+# imports
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from schemas import  EncText, DecText
-# from controllers import TextController
+from controllers import TextController
 
+# app initialization
 app: FastAPI = FastAPI()
 app.add_middleware(CORSMiddleware, allow_origins=['http://localhost:5173'],
                     allow_credentials=True,
                     allow_methods=["POST", "GET"],
                     allow_headers=["*"],
                 )
-# text_controller: TextController = TextController()
+
+# controller initialization
+text_controller: TextController = TextController()
 
 
+# routes
 @app.get("/api/")
 async def home() -> dict[str, str]:
     return {"message": "Hello, World!"}
 
 
 @app.post("/api/enc")
-async def encrypt_text(text: EncText) -> dict[str, EncText]:
-    # print(text)
-    return {"returned: ": text}
+async def encrypt_text(text: EncText) -> dict[str, str]:
+    cipher_text: str = text_controller.encrypt(text.key, text.plainText)
+    return {"cipherText": cipher_text}
+
+
+@app.post("/api/dec")
+async def decrypt_text(text: DecText) -> dict[str, DecText]:
+    return {"returned": text}
